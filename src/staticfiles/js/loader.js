f8c014a4a93deb851ada421cd89d5730f06eaa15
Carlos Eduardo <eduardokadu831@gmail.com>
2023-10-24 18:00:36 -0300

    add staticfiles

---
+++ src/staticfiles/js/loader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        var loader = document.getElementById('loader');
+        loader.style.display = 'none';
+    }, 1000);
+});
