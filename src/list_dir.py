5e2e2335e9b2f5500f29dd4b2043ec16f033c7bc
Carlos Eduardo <eduardokadu831@gmail.com>
2023-11-29 19:50:05 -0300

    correções de rastreamento

--- src/list_dir.py
+++ src/list_dir.py
@@ -15,8 +15,11 @@ def listar_arquivos(diretorio):
 
 def salvar(arquivos, caminho_txt):
     try:
+        arquivospdf = [arquivo for arquivo in arquivos if arquivo != "nao-remova.txt"]
+
         with open(caminho_txt, 'w') as arquivo_txt:
-            arquivo_txt.write('\n'.join(arquivos))
+            arquivo_txt.write('\n'.join(arquivospdf))
+            
         print("Lista de arquivos salva.")
     except Exception as e:
         print(f"Erro ao salvar em txt: {e}")
