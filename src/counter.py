dcf4492f62acdd9a24f2e90cefba344285925845
Carlos Eduardo <eduardokadu831@gmail.com>
2023-12-07 20:37:46 -0300

    correções

--- src/counter.py
+++ src/counter.py
@@ -1,17 +1,15 @@
-import json
 from collections import defaultdict
 from datetime import datetime
+import json
 import os
 
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 
-# Função para ler dados do novo formato de JSON
 def ler_dados_novo_json(nome_arquivo):
     with open(nome_arquivo, "r", encoding="utf-8") as arquivo_json:
         dados_json = json.load(arquivo_json)
     return dados_json
 
-# Função para extrair dados do novo formato de JSON
 def extrair_dados_novo_json(dados_pdf):
     contagem = defaultdict(lambda: defaultdict(int))
     total_todos = defaultdict(int)
@@ -26,20 +24,20 @@ def extrair_dados_novo_json(dados_pdf):
         mes = datetime.strptime(pdf_data, "%Y-%m-%d").strftime("%b")
         ano = datetime.strptime(pdf_data, "%Y-%m-%d").strftime("%Y")
 
+        # Incrementar as contagens corretamente
         contagem[municipio][mes] += 1
         total_todos[mes] += 1
 
-        saida_json.append({"municipio": municipio, "mes": mes, "qtd": 1, "ano": ano})
+    # Criar a lista de saída com as contagens corretas
+    for municipio, meses in contagem.items():
+        for mes, qtd in meses.items():
+            saida_json.append({"municipio": municipio, "mes": mes, "qtd": qtd, "ano": ano})
 
     return saida_json
 
-# Ler dados do novo formato de JSON de entrada
 dados_pdf_novo_entrada = ler_dados_novo_json(diretorio_atual + "/dados/data.json")
-
-# Extrair dados do novo formato de JSON
 saida_json_novo = extrair_dados_novo_json(dados_pdf_novo_entrada)
 
-# Salvar em um arquivo JSON com codificação correta
 with open(diretorio_atual + "/dados/counter.json", "w", encoding="utf-8") as arquivo_saida:
     json.dump(saida_json_novo, arquivo_saida, indent=2, ensure_ascii=False)
 
